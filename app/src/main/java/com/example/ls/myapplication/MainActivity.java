10ea3c3b4eb1fc32b82b19fa8d08c6d3c68d1f6b
liangshi <ls200201020@163.com>
2017-11-25 12:29:43 +0800

    1、first commit

---
+++ app/src/main/java/com/example/ls/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ls.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        List<String> list = new ArrayList<>();
+    }
+}
---
+++ app/src/main/java/com/example/ls/myapplication/Math.java
@@ -0,0 +1,11 @@
+package com.example.ls.myapplication;
+
+/**
+ * Created by LS on 2017/11/20.
+ */
+
+public class Math {
+    public int f(int n){
+        return n+1;
+    }
+}
